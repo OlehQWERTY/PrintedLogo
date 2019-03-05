c041a6b9b38c24e9ed5fd50b465b21bf1f5dff52
Unknown <oleggordiichenko@gmail.com>
2019-03-05 11:29:01 +0200

    debug message added

--- PrintedLogo.ino
+++ PrintedLogo.ino
@@ -4,7 +4,7 @@
 #define RelOut        4
 // set var
 #define MAX_DISTANCE 30
-#define DELTA_CM     2.0  // 0.45 0.5 0.8
+#define DELTA_CM     1.4  // 0.45 0.5 0.8
 #define distMeasurementsAmmount 5
 
 float zeroPointCm = 0.0;
@@ -42,7 +42,7 @@ float initProcedure() {
 		digitalWrite(LED, LOW);
 	}
 	avarageDist = avarageDist/4.0;
-
+	Serial.println("init is done!");
 	return avarageDist;
 }
 
@@ -70,23 +70,30 @@ void setup() {
 
 void loop() {
 	//Serial.println(IRAverageDist());
+	Serial.println("Debug point 1");
 	if(soleChecker(zeroPointCm, IRAverageDist(), DELTA_CM))
 	{
+		Serial.println("Debug point 2");
 		if(state_LED != true)
 		{
+			Serial.println("Debug point 3");
 			int tmp = 0;  // if random value error counter
 			// error here ???
 			for(int i = 0; i < 8; i++) // 10 * delay(50) = 500 wait after someone putted suole
 			{
+				Serial.println("Debug point 4");
 				CurrentDistCm = IRAverageDist();
+				Serial.println("Debug point 5");
 				delay(50);
 				if(abs(CurrentDistCm - IRAverageDist()) < DELTA_CM)
 				//if(abs(zeroPointCm - IRAverageDist()) < DELTA_CM)
 				{
+					Serial.println("Debug point 6");
 					flag = true;
 				}
 				else
 				{
+					Serial.println("Debug point 7");
 					// flag = false;
 					// Serial.println("flag = false!!!");
 					tmp++;
@@ -95,28 +102,37 @@ void loop() {
 				// set up this counter
 				if(tmp >= 2)  // if ammount of random value error > 2 
 				{
+					Serial.println("Debug point 8");
 					flag = false;
 					Serial.println("flag = false...");
 					break;
 				}
 			}
-			
+			Serial.println("Debug point 9");
 			if(flag)
 			{
+				Serial.println("Debug point 10");
 				digitalWrite(RelOut, HIGH); // спрацювання клапана
-				//Serial.println("(if) RelOut HIGH");
+				Serial.println("(if) RelOut HIGH");
 				delay(200); // 200 ms працює клапан
+				Serial.println("Debug point 11");
 				digitalWrite(RelOut, LOW);
-				// Serial.println("(if) RelOut LOW");
+				Serial.println("(if) RelOut LOW");
 				state_LED = true; // ??? why we need it?
+				Serial.println("Debug point 12");
 				delay(3000); // затримка від хибних спрацювань під час відпрацювання циклу машини (цикл машини 3,5 с)
+				Serial.println("Debug point 13");
 			}
 		}
 	}
 	else
 	{
+		Serial.println("Debug point 14");
 		digitalWrite(RelOut, LOW);
-		// Serial.println("(else) RelOut LOW");
+		Serial.println("(else) RelOut LOW");
+		Serial.println("Debug point 15");
 		state_LED = false;
+		Serial.println("Debug point 16");
 	}
+	Serial.println("Debug point 17");
 }
